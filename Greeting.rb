589f383f4afa37725e3c13fb726807dcc4110041
Douglas Crews <douglas.crews@partech.com>
2023-07-10 17:28:15 -0700

    https://www.tutorialspoint.com/ruby/ruby_classes.htm

--- Greeting.rb
+++ Greeting.rb
@@ -8,24 +8,34 @@ END {
 
 class Greeter
 	attr_accessor :names
+	attr_accessor :foobar # instance variable; starts with @
 
-	def initialize(names = "World")
+	@@class_name = "Greeter" # class variable; starts with @@
+
+	$global_name = "This is weird" # global variable; available everywhere?; starts with $
+
+	def initialize(names = "World", foobar = "foo")
+		puts "initialize(#{names}, #{foobar})"
 		@names = names
+		@foobar = foobar
 	end
 
-	def hello
+	def hello # method names should be all lowercase
+		puts "hello()"
+		message = "Fuck Off" # local variable within method; starts with underscore or letter
 		if @names.nil? # is null
 			puts "..."
 		elsif @names.respond_to?("each") # is an iterator
 			@names.each do |name|
-				puts "Fuck off, #{name}!" # error in tutorial ruby-lang.org/.../quickstart/3
+				puts "#{message}, #{name}!" # error in tutorial ruby-lang.org/.../quickstart/3
 			end
 		else # is singular
-			puts "Fuck off, #{@names}!"
+			puts "#{message}, #{@names}!"
 		end
 	end
 
 	def bye
+		puts "bye()"
 		if @names.nil?
 			puts "..."
 		elsif @names.respond_to?("join")
@@ -36,6 +46,7 @@ class Greeter
 	end
 
 	def print_multiline
+		puts "print_multiline()"
 		print <<EOF
 		This prints a multiline
 		statement.
@@ -49,6 +60,8 @@ EOF
 		print <<`EOC` # execute commands
 		echo "First echoed message."
 		echo "Second echoed message." # Why is this not printing?
+		ls
+		ls -la
 EOC
 
 		print <<"first", <<"second" # stacked
@@ -56,6 +69,23 @@ EOC
 first
 		I said bar.
 second
+	end
+
+	def instanceVar
+		puts "instanceVar()"
+		puts @foobar
+		@foobar = "bar"
+		puts @foobar
+	end
+
+	def classVar
+		puts "This is #{@@class_name}. Global variable is #{$global_name}."
+	end
+end
+
+class Nerf
+	def print_global_variable
+		puts "Global variable is #{$global_name}."
 	end
 end
 
@@ -78,5 +108,18 @@ if __FILE__ == $0
 	g.bye
 =end
 
-	g.print_multiline
+	# g.print_multiline
+
+	# g.instanceVar
+
+	g2 = Greeter.new(foobar = "bar")
+	# g2.foobar = "bar"
+	# g2.instanceVar
+
+	g.classVar
+
+	g2.classVar
+
+	n = Nerf.new
+	n.print_global_variable
 end
